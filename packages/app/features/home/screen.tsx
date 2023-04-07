5a9fb0e026006e40a66b3776675ab773727ebaf0
Desmond Tatilian <info@launchthatbrand.com>
2023-04-07 17:39:17 -0400

    added packages/features/app and replaced nextjs index with a solito page

---
+++ packages/app/features/home/screen.tsx
@@ -0,0 +1,10 @@
+import { TextLink } from "solito/link";
+import { View } from "react-native";
+
+export function HomeScreen() {
+  return (
+    <View>
+      <TextLink href="/">THIS IS HOMESCREEN</TextLink>
+    </View>
+  );
+}
---
+++ packages/app/features/screen.tsx
@@ -0,0 +1,10 @@
+import { TextLink } from "solito/link";
+import { View } from "react-native";
+
+export function SolitoScreen() {
+  return (
+    <View>
+      test
+    </View>
+  );
+}
