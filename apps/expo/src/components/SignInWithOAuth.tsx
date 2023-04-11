88177946744ee9529dd2b3340886f9900f713762
Desmond Tatilian <info@launchthatbrand.com>
2023-04-11 16:01:22 -0400

    Revert "expo working without tailwind styles. I had to add --clear flag to expo dev script to clear nativewind out of expo cache"
    This reverts commit 4378613fe11128c56f65c47299b6e9f8c6422e38.

--- apps/expo/src/components/SignInWithOAuth.tsx
+++ apps/expo/src/components/SignInWithOAuth.tsx
@@ -16,9 +16,8 @@ const SignInWithOAuth = () => {
         setActive({ session: createdSessionId });
       } else {
         // Modify this code to use signIn or signUp to set this missing requirements you set in your dashboard.
-        throw new Error(
-          "There are unmet requirements, modifiy this else to handle them",
-        );
+        throw new Error("There are unmet requirements, modifiy this else to handle them")
+
       }
     } catch (err) {
       console.log(JSON.stringify(err, null, 2));
@@ -29,11 +28,11 @@ const SignInWithOAuth = () => {
   return (
     <View className="rounded-lg border-2 border-gray-500 p-4">
       <Button
-        title="Sign in with Discord!"
+        title="Sign in with Discord"
         onPress={handleSignInWithDiscordPress}
       />
     </View>
   );
-};
+}
 
 export default SignInWithOAuth;
