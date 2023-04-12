907be2b63a4465d2894db6ccf4e63e8e832c6611
Desmond <desmond@Desmonds-MacBook-Pro.local>
2023-04-12 10:07:56 -0400

    t3 homescreen on solito

--- packages/app/design/layout.tsx
+++ packages/app/design/layout.tsx
@@ -1,4 +1,6 @@
-import { View } from 'react-native'
-import { styled } from 'nativewind'
+import { View } from "react-native";
+import { styled } from "nativewind";
+import { SafeAreaView as ReactNativeSafeAreaView } from "react-native-safe-area-context";
 
-export const Row = styled(View, "flex-row")
+export const Row = styled(View, "flex-row");
+export const SafeAreaView = styled(ReactNativeSafeAreaView);
--- packages/app/features/home/screen.tsx
+++ packages/app/features/home/screen.tsx
@@ -1,10 +1,50 @@
 import { TextLink } from "solito/link";
 import { View } from "../../design/view";
+import { Button } from "../../design/button";
+import { Text } from "../../design/typography";
+import { FlashList } from "@shopify/flash-list";
+import { useAuth } from "@clerk/clerk-expo";
+import { SafeAreaView } from "../../design/layout";
+
+const SignOut = () => {
+  const { signOut } = useAuth();
+  return (
+    <View className="rounded-lg border-2 border-gray-500 p-4">
+      <Button
+        title="Sign Outs"
+        onPress={() => {
+          signOut();
+        }}
+      />
+    </View>
+  );
+};
 
 export function HomeScreen() {
   return (
-    <View className="flex-1 items-center justify-center">
-      <TextLink href="/solito">👈 Go Solito</TextLink>
-    </View>
+    <SafeAreaView className="bg-[#2e026d] bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+      <View className="h-full w-full p-4">
+        <Text className="mx-auto pb-2 text-5xl font-bold text-white">
+          Create <Text className="text-[#cc66ff]">T3</Text> Turbo
+        </Text>
+
+        <View className="py-2"></View>
+
+        {/* <FlashList
+          data={postQuery.data}
+          estimatedItemSize={20}
+          ItemSeparatorComponent={() => <View className="h-2" />}
+          renderItem={(p) => (
+            <TouchableOpacity onPress={() => setShowPost(p.item.id)}>
+              <PostCard post={p.item} />
+            </TouchableOpacity>
+          )}
+        /> */}
+
+        {/* <CreatePost /> */}
+        <SignOut />
+        <TextLink href="/solito">Regular Link</TextLink>
+      </View>
+    </SafeAreaView>
   );
 }
