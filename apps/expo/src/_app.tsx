b517d136ba231d523a5ac55b9d526c3279d0d583
Desmond Tatilian <info@launchthatbrand.com>
2023-04-12 14:38:51 -0400

    Revert "basic navigation working"
    This reverts commit 2ea1793b8e21e09041439c0f968a129fb659a723.

--- apps/expo/src/_app.tsx
+++ apps/expo/src/_app.tsx
@@ -3,13 +3,20 @@ import React from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { TRPCProvider } from "./utils/trpc";
 
+import { HomeScreen } from "./screens/home";
 import { SignInSignUpScreen } from "./screens/signin";
 import { ClerkProvider, SignedIn, SignedOut } from "@clerk/clerk-expo";
 import { tokenCache } from "./utils/cache";
 import Constants from "expo-constants";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import { NativeNavigation } from "@acme/app/navigation/native";
 import { Provider } from "@acme/app/provider";
+
+const Stack = createNativeStackNavigator<{
+  home: undefined;
+  solito: undefined;
+}>();
 
 export const App = () => {
   return (
--- apps/expo/src/components/SignInWithOAuth.tsx
+++ apps/expo/src/components/SignInWithOAuth.tsx
@@ -10,16 +10,14 @@ const SignInWithOAuth = () => {
 
   const handleSignInWithDiscordPress = React.useCallback(async () => {
     try {
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
       const { createdSessionId, signIn, signUp, setActive } =
         await startOAuthFlow();
       if (createdSessionId) {
         setActive({ session: createdSessionId });
       } else {
         // Modify this code to use signIn or signUp to set this missing requirements you set in your dashboard.
-        throw new Error(
-          "There are unmet requirements, modifiy this else to handle them",
-        );
+        throw new Error("There are unmet requirements, modifiy this else to handle them")
+
       }
     } catch (err) {
       console.log(JSON.stringify(err, null, 2));
@@ -35,6 +33,6 @@ const SignInWithOAuth = () => {
       />
     </View>
   );
-};
+}
 
 export default SignInWithOAuth;
