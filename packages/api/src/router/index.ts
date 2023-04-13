de7abb5dbd6d5cf03e194dc3acac7d7f55feb5ff
Desmond Tatilian <info@launchthatbrand.com>
2023-04-12 20:50:33 -0400

    removed entry

--- packages/api/src/router/index.ts
+++ packages/api/src/router/index.ts
@@ -1,11 +1,9 @@
 import { router } from "../trpc";
-import { entryRouter } from "./entry";
 import { authRouter } from "./auth";
 import { userRouter } from "./user";
 import { postRouter } from "./post";
 
 export const appRouter = router({
-  entry: entryRouter,
   user: userRouter,
   auth: authRouter,
   post: postRouter,
