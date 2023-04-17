3e887ecae9214406cd500b728fe733ec552d6bbe
Desmond Tatilian <info@launchthatbrand.com>
2023-04-17 16:54:53 -0400

    added expo drawer nav plugins

--- apps/expo/babel.config.js
+++ apps/expo/babel.config.js
@@ -7,7 +7,11 @@ module.exports = function (api) {
   process.env.EXPO_ROUTER_APP_ROOT = "../../apps/expo/src/app";
 
   return {
-    plugins: ["nativewind/babel", require.resolve("expo-router/babel")],
+    plugins: [
+      "nativewind/babel",
+      "react-native-reanimated/plugin",
+      require.resolve("expo-router/babel"),
+    ],
     presets: [["babel-preset-expo", { jsxRuntime: "automatic" }]],
   };
 };
