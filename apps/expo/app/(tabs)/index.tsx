c3dc0958c847f96a7d99cab3646cb64ad483ac3b
Desmond <desmond@Desmonds-MacBook-Pro.local>
2023-04-28 17:28:31 -0400

    moved expo files to src folder

--- apps/expo/app/(tabs)/index.tsx
+++
@@ -1,5 +0,0 @@
-import { HomeScreen } from "@acme/app/features/home/screen";
-
-export default function Home() {
-  return <HomeScreen />;
-}
--- apps/expo/babel.config.js
+++ apps/expo/babel.config.js
@@ -1,12 +1,17 @@
+/** @type {import("@babel/core").ConfigFunction} */
 module.exports = function (api) {
-  api.cache(true);
+  api.cache.forever();
+
+  // Make Expo Router run from `src/app` instead of `app`.
+  // Path is relative to `/node_modules/expo-router`
+  process.env.EXPO_ROUTER_APP_ROOT = "../../apps/expo/src/app";
+
   return {
-    presets: [["babel-preset-expo", { jsxRuntime: "automatic" }]],
     plugins: [
       "nativewind/babel",
       "react-native-reanimated/plugin",
-      // https://expo.github.io/router/docs/#configure-the-babel-plugin
       require.resolve("expo-router/babel"),
     ],
+    presets: [["babel-preset-expo", { jsxRuntime: "automatic" }]],
   };
 };
