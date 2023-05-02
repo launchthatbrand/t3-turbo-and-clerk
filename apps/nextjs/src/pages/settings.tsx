fdfcb2d9047fd9260c0e33347a3d7cd23a91a7ea
Desmond <desmond@Desmonds-MacBook-Pro.local>
2023-05-02 16:33:48 -0400

    update

---
+++ apps/nextjs/src/pages/settings.tsx
@@ -0,0 +1,10 @@
+import { SettingsScreen as FeatureSettingsScreen } from "@acme/app/features/settings/screen";
+
+import MainLayout from "@acme/app/layouts/MainLayout";
+export default function SettingsScreen() {
+  return <FeatureSettingsScreen />;
+}
+
+SettingsScreen.getLayout = function getLayout(page: React.ReactNode) {
+  return <MainLayout>{page}</MainLayout>;
+};
---
+++ packages/app/features/settings/screen.tsx
@@ -0,0 +1,20 @@
+import { Link } from "solito/link";
+import { Button } from "../../design/button";
+import { Text } from "../../design/typography";
+import { View } from "../../design/view";
+
+import { CardSettings } from "../../design/components/CardsOld/CardSettings.js";
+import CardProfile from "../../design/components/CardsOld/CardProfile.js";
+
+export function SettingsScreen() {
+  return (
+    <View className="flex flex-row flex-wrap">
+      <View className="w-full px-4 lg:w-8/12">
+        <CardSettings />
+      </View>
+      <View className="w-full px-4 lg:w-4/12">
+        <CardProfile />
+      </View>
+    </View>
+  );
+}
