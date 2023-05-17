7c4e6324524e1ba07bad7ae66bff99544b1f7c51
Desmond <desmond@Desmonds-MacBook-Pro.local>
2023-05-17 12:03:35 -0400

    minor change to trigger build

--- packages/app/features/solito/screen.tsx
+++ packages/app/features/solito/screen.tsx
@@ -8,7 +8,7 @@ export function SolitoScreen() {
   return (
     <View className="flex-1 items-center justify-center">
       <Text className="mx-auto pb-2 text-5xl font-bold text-white">
-        Solito Screen
+        Solito Screens
       </Text>
       <Link href="/">
         <View className="rounded-lg border-2 border-gray-500 bg-slate-400 p-4">
